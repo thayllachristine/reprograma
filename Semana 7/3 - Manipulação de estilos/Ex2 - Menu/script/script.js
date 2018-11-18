60db5bc24f808c09959311b545345a85147fcdef
thayllachristine <thayllachristineo@gmail.com>
2018-11-18 14:28:50 -0200

    Ex2 - Menu: Manipulação de estilos

---
+++ Semana 7/3 - Manipulação de estilos/Ex2 - Menu/script/script.js
@@ -0,0 +1,23 @@
+const menuGeneral = document.querySelector(`.sidebar-menu`)
+
+const menuButton = document.querySelector(`.sidebar-menu__button`)
+
+menuButton.addEventListener(`click`, function (event) {
+    event.preventDefault()
+
+    // if (menuGeneral.style.left === `-34%`){ 
+    //     menuGeneral.style.left = `0%`
+    // } else {
+    //     menuGeneral.style.left = `-34%`
+    // }
+
+    let verifyPosition = (menuGeneral.style.left)
+
+    if (verifyPosition === `-430px`) {
+        menuGeneral.style.left = `0px`
+        console.log(`1 : verifyPosition = -0px`)
+    } else {
+        menuGeneral.style.left = `-430px`
+        console.log(`2 : verifyPosition = -430px`)
+    }
+})
